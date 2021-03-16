38a5e51031843c06ed228e1fad7f3905e73616d6
Mirmehdi Suleymanov <suleymanovm066@gmail.com>
2021-03-16 20:48:09 +0400

    Blog seyfesinde 2 section hazirlandi

--- Myprojects/js/s.js
+++ Myprojects/js/s.js
@@ -1,8 +1,36 @@
-a=prompt("a :");
+
+
+/*
+let qeydiyat={
+    ad:prompt("Adiniz ? ")
+   
+}
+a=qeydiyat.ad
+console.log(alert("Salam " + a  +" " +  "Bey")); */
+
+
+/*var a=prompt("daxil ed:");
+
+if(a==''){
+    console.log(alert("Bosdur"));
+}
+else if(a = String){
+    console.log(alert("qeseyy"));
+} 
+else if(a = String){
+    console.log(alert("y"));
+} 
+else{
+    
+    console.log(alert(Number(a)));
+} */
+
+
+/*a=prompt("a :");
 b=prompt("b :")
 
 c=Number(a)+Number(b)
-console.log(alert("cavab :" + c));
+console.log(alert("cavab :" + c));*/
 // 1 ci a ya input aliriq
 // 2ci b ye input aliriq
 // 3 aldiqimiz inputu Nubere cevirik Number() yle 
