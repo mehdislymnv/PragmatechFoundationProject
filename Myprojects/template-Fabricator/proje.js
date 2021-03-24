cefefc396279b0b1ed8d2ba4a2130d6887ce3ee4
Mirmehdi Suleymanov <suleymanovm066@gmail.com>
2021-03-24 18:46:56 +0400

    Blog ve Blog2 seyfeleri risponsivlik elave edildi
    cd

--- Myprojects/template-Fabricator/proje.js
+++ Myprojects/template-Fabricator/proje.js
@@ -15,13 +15,34 @@ function kapat(){
     document.getElementById("burger-menu").style.width="0"
 }
 /**///////////////////*/ 
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+/* 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
 if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "100px";} 
-else {
+    document.getElementById("navbar").style.display = "block";
+    document.getElementById("navbar").style.top = "0px"; 
+}
+    
+else 
+{
     document.getElementById("navbar").style.top = "0";
+   
+    
+    
 }
 prevScrollpos = currentScrollPos;
 }
+*/
