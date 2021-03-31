de5bf97e3ba4486a0f5c4b7e52f73081961fc4b8
Mirmehdi Suleymanov <suleymanovm066@gmail.com>
2021-03-31 23:51:49 +0400

    sldier arasdirildi tekar duzeldildi

--- lessonpractice/js/js/slider.js
+++ lessonpractice/js/js/slider.js
@@ -4,18 +4,42 @@ let sliderContent=document.querySelector('.slider-content');
 let slides=document.querySelectorAll('.slide');
 let leftBtn=document.querySelector('.leftBtn')
 let rightBtn=document.querySelector('.rightBtn')
+
 pos=0;
+index=0;
+
 for(i=0;i<slides.length;i++){
     slides[i].style.width=containerWidth+'px';
 }
 
+
 sliderContent.style.width=containerWidth*slides.length+'px';
 
 leftBtn.addEventListener('click',function(){
-    sliderContent.style.transform=`translateX(${pos}px)`
-    pos+=containerWidth;
+
+   
+        if(index > 0){
+            index--
+            pos+=containerWidth;
+            sliderContent.style.transform=`translateX(${pos}px)`
+           
+        
+        }
+       
+
+        
+        
+    
 })
+    
+
 rightBtn.addEventListener('click',function(){
-    sliderContent.style.transform=`translateX(${pos}px)`
-    pos-=containerWidth;
+    if(index < slides.length -1){
+        index++
+        pos-=containerWidth;
+        sliderContent.style.transform=`translateX(${pos}px)`
+       
+    
+    }
+
 })
