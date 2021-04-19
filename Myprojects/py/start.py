3c00b42f00b641a08b4f2c570c83919eb0b1e33c
Mirmehdi Suleymanov <suleymanovm066@gmail.com>
2021-04-19 22:03:21 +0400

    py tapsiriqlari elave edildi

---
+++ Myprojects/py/start.py
@@ -0,0 +1,46 @@
+
+for i in range(101):
+    if i%2==1:
+        print(i)
+    
+#cem=0
+
+#2
+# for i in range(101):
+    
+#    if i%2==1:
+#        cem+=i
+# print (cem)
+#3     
+"""
+cumle="Javascript ne vaxtdan proqramlasdirma dili olub?"
+
+a=cumle.split()
+cem=0
+for i in a:
+    cem+=len(i)
+print(cem)
+
+#5
+
+soyad="Suleymanov"[::-1]
+print(soyad)
+"""
+#4
+
+cumle="Javascript ne vaxtdan proqramlasdirma dili olub?"
+a=cumle.split()
+print(len(a))
+
+
+#6
+"""
+sait=["a","e","i","o","u"]
+cumle="Javascript ne vaxtdan proqramlasdirma dili olub?"
+
+say=""
+for i in sait:
+    for j in cumle:
+        if i==j:
+            say += i
+print(say)"""
