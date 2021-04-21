2d9256bf5711eee7494263ac19184de9e7c6b517
Mirmehdi Suleymanov <suleymanovm066@gmail.com>
2021-04-21 22:01:12 +0400

    Tipler aradirildi ve elave olundu

--- Myprojects/py/start.py
+++ Myprojects/py/start.py
@@ -1,8 +1,8 @@
-
+""" 
 for i in range(101):
     if i%2==1:
         print(i)
-    
+     """
 #cem=0
 
 #2
@@ -27,11 +27,11 @@ soyad="Suleymanov"[::-1]
 print(soyad)
 """
 #4
-
+""" 
 cumle="Javascript ne vaxtdan proqramlasdirma dili olub?"
 a=cumle.split()
 print(len(a))
-
+ """
 
 #6
 """
@@ -44,3 +44,72 @@ for i in sait:
         if i==j:
             say += i
 print(say)"""
+
+""" a=input("Adinizi girin : ")
+
+print("salam {}  ".format(a)) """
+
+
+""" s=[50,35,"a"]
+cem=0
+for i in s:
+    if type(i)==str:
+        print("prablem var")
+    else:
+      cem+=i
+print(cem) 
+
+
+"""
+
+
+
+""" a=[25,33,0,40,30]
+b=[10,25,33,5,10]
+
+def tap():
+    for i in a:
+        if i in b:
+            return True
+        else:
+            return False
+               
+    
+     
+
+print(tap()) """
+
+""" a=45321
+c=str(a)
+cem=0
+for i in c:
+    cem+=int(i)
+print(cem)
+
+
+num = 45342
+sum=0
+while num>0:
+    qaliq = num%10 
+    num = num//10 
+    sum+=qaliq 
+print(sum)
+ """
+""" 
+import random
+lista=["Mehdi","Fikret","Emin","Nicat"]
+
+random.shuffle(lista)
+  
+
+
+
+print(lista)
+list = ['Red', 'Blue', 'Green', 'White', 'Black']
+
+print(random.choice(list)) """
+
+N = int(input(" : "))
+arr= [] 
+arr.insert(0,N)
+print(arr)
---
+++ Question.md
@@ -0,0 +1,17 @@
+- List
+    list [] ile yaradilir uzunluqu olculur deyisdirile bilir muraciyet etmek olur silinir tipi muyeyen oluna bilir evez oluna biler
+    diger 2 tipe nezeren daha esnekdir, tam ,sinvol ve mentiqsel deyerleri ala bilir.
+    yeni bir list yaradmaq ucun list("a",,,,,)  bunuda etmek mumkudur
+- Tuple
+    Tuple da () icinde yazilir elemetler, qisaltmasi "tup" dir.
+    Listden ferqi ondadir ki burda elemetler deyisdirile bilmir.
+    siralanmir evaz olunmur silinmir ve deyisdirilmir lakin DICT den ferqli olaraq Burda eyni parametir ola biler yeni tekrar oluna biler
+
+- Dictionary
+    key:value  mentiqi ile isleyen {} icinde yazilir LISTden ferqli olaraq.qisatmasi "disct" dir 
+    siralanir ve deyisdirmek olur ,uzunluqu , muraciyet oluna bilesi,tipi muyeyen oluna bilinir
+    LIST kimu uzun ve ya boyuk melumatlar ozunde barindirmasada bir nesnenin ayrintili xaselerini,parametirlerini ozunde saxlaya bilir
+    user={
+        "ad"    : "mehdi",
+        "soyad" : "Suleymanov"
+    } 
